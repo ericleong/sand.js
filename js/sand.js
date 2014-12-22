5fc64411a72b482c036980d19ef588f18a868c3f
Eric Leong <eric@ericleong.me>
2014-12-21 19:23:55 -0500

    Basic sand game with no slipping.

--- js/sand.js
+++ js/sand.js
@@ -1,14 +1,53 @@
 var canvas;
 var gl;
+var rectTexture;
+var sandTexture0, sandTexture1;
+
+var sandBuffer;
+var rectFrameBuffer;
+var sandFrameBuffer0, sandFrameBuffer1;
+
 var rectVerticesBuffer;
 var rectVerticesTextureCoordBuffer;
-var mvMatrix;
 var shaderProgram;
 var vertexPositionAttribute;
 var textureCoordAttribute;
-var perspectiveMatrix;
 
-var IDENTITY_MATRIX = new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+function initUserInput() {
+	var canvas = document.getElementById('glcanvas');
+	
+	// create a new canvas
+	var painter = document.createElement('canvas');
+	var context = painter.getContext('2d');
+	context.imageSmoothingEnabled = false;
+
+	// set dimensions
+	painter.width = canvas.width;
+	painter.height = canvas.height;
+
+	function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
+	}
+
+	canvas.addEventListener('mousemove', function(evt) {
+		var mousePos = getMousePos(canvas, evt);
+
+		context.beginPath();
+		context.arc(mousePos.x, mousePos.y, 8, 0, 2 * Math.PI, false);
+		context.fillStyle = 'rgba(255, 255, 255, 1.0)';
+		context.fill();
+
+		gl.bindTexture(gl.TEXTURE_2D, rectTexture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, painter);
+
+		context.clearRect(0, 0, painter.width, painter.height);
+	}, false);
+}
 
 //
 // start
@@ -25,9 +64,9 @@ function start() {
 	
 	if (gl) {
 		gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-		gl.clearDepth(1.0);                 // Clear everything
-		gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-		gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+		gl.enable(gl.BLEND);
+		gl.disable(gl.DEPTH_TEST);
+		gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
 		
 		// Initialize the shaders; this is where all the lighting for the
 		// vertices and so forth is established.
@@ -42,10 +81,8 @@ function start() {
 		// Next, load and set up the textures we'll be using.
 
 		initTextures();
-		
-		// Set up to draw the scene periodically.
-		
-		setInterval(drawScene, 15);
+
+		initUserInput();
 	}
 }
 
@@ -60,8 +97,8 @@ function initWebGL() {
 	
 	try {
 		gl = canvas.getContext('experimental-webgl');
-	}
-	catch(e) {
+	} catch(e) {
+		console.log(e);
 	}
 	
 	// If we don't have a GL context, give up now
@@ -125,10 +162,13 @@ function initBuffers() {
 // Draw the scene.
 //
 function drawScene() {
-	// Clear the canvas before we start drawing on it.
+	sandBuffer = sandBuffer == 0 ? 1 : 0;
 
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	
+	gl.bindFramebuffer(gl.FRAMEBUFFER, sandBuffer == 0 ? sandFrameBuffer0 : sandFrameBuffer1);
+
+	// Clear the canvas before we start drawing on it.
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
 	// Draw the rect by binding the array buffer to the rect's vertices
 	// array, setting attributes, and pushing it to GL.
 
@@ -141,18 +181,54 @@ function drawScene() {
 	gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
 	
 	// Specify the texture to map onto the face.
-	
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, rectTexture);
 	gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uSampler'), 0);
 	
-	var pUniform = gl.getUniformLocation(shaderProgram, 'uPMatrix');
-	gl.uniformMatrix4fv(pUniform, false, IDENTITY_MATRIX);
-
-	var mvUniform = gl.getUniformLocation(shaderProgram, 'uMVMatrix');
-	gl.uniformMatrix4fv(mvUniform, false, IDENTITY_MATRIX);
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, sandBuffer == 0 ? sandTexture1 : sandTexture0);
+	
+	gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uAdvance'), 1);
 
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+	// draw user input
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, rectTexture);
+
+	gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uAdvance'), 0);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+	// clear user input
+	gl.bindFramebuffer(gl.FRAMEBUFFER, rectFrameBuffer);
+
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// draw onto screen
+	gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// Draw the rect by binding the array buffer to the rect's vertices
+	// array, setting attributes, and pushing it to GL.
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, rectVerticesBuffer);
+	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+	// Set the texture coordinates attribute for the vertices.
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, rectVerticesTextureCoordBuffer);
+	gl.vertexAttribPointer(textureCoordAttribute, 2, gl.FLOAT, false, 0, 0);
+
+	gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uSampler'), 0);
+
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, sandBuffer == 0 ? sandTexture0 : sandTexture1);
+
+	gl.uniform1i(gl.getUniformLocation(shaderProgram, 'uAdvance'), 0);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+	window.requestAnimationFrame(drawScene);
 }
 
 //
@@ -247,19 +323,43 @@ function getShader(gl, id) {
 }
 
 function initTextures() {
-	rectTexture = gl.createTexture();
 	rectImage = new Image();
-	rectImage.onload = function() { handleTextureLoaded(rectImage, rectTexture); }
+	rectImage.onload = function() { handleTextureLoaded(rectImage); }
 	rectImage.src = './sand.png';
 }
 
-function handleTextureLoaded(image, texture) {
+function initTexture(image) {
+	var texture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+	var framebuffer = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+
+	return [texture, framebuffer];
+}
+
+function handleTextureLoaded(image) {
+	var tex = initTexture(image);
+	rectTexture = tex[0];
+	rectFrameBuffer = tex[1];
+	
+	tex = initTexture(image);
+	sandTexture0 = tex[0];
+	sandFrameBuffer0 = tex[1];
+	
+	tex = initTexture(image);
+	sandTexture1 = tex[0];
+	sandFrameBuffer1 = tex[1];
+
 	gl.bindTexture(gl.TEXTURE_2D, null);
+
+	// Set up to draw the scene periodically.
+	window.requestAnimationFrame(drawScene);
 }
