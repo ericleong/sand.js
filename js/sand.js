5eba3d8948f35491e85557253a26d426563fef4f
Eric Leong <eric@ericleong.me>
2014-12-26 21:20:42 -0500

    Reorganize user input code into separate file.

--- js/sand.js
+++ js/sand.js
@@ -1,14 +1,11 @@
-var canvas;
 var gl;
 
 // textures
-var rectTexture, maskTexture;
 var sandTexture0, sandTexture1;
 var cellsTexture, rulesTexture;
 
 // frame buffers
 var sandBuffer;
-var rectFrameBuffer, maskFrameBuffer;
 var sandFrameBuffer0, sandFrameBuffer1;
 
 // vertex/coordinate buffers
@@ -28,15 +25,10 @@ var uBias;
 var aCopyVertexPosition;
 var aCopyTextureCoord;
 
-// draw input hint
-var updateInput = false;
-
 //
-// start
+// init
 //
-function start() {
-	canvas = document.getElementById('glcanvas');
-
+function init(canvas, config) {
 	initWebGL(canvas);      // Initialize the GL context
 	
 	// Only continue if WebGL is available and working
@@ -56,104 +48,10 @@ function start() {
 		
 		initBuffers();
 
-		// Next, load and set up the textures we'll be using.
+		// Load and set up the textures we'll be using.
 
-		initTextures();
-
-		initUserInput();
+		initTextures(canvas, config);
 	}
-}
-
-function initUserInput() {
-	var down = false;
-	var color = 'rgba(255, 255, 255, 1.0)';
-
-	// create a new canvas
-	var painter = document.createElement('canvas');
-	var mask = document.createElement('canvas');
-
-	// set dimensions
-	mask.width = canvas.width;
-	mask.height = canvas.height;
-
-	painter.width = canvas.width;
-	painter.height = canvas.height;
-
-	function getMousePos(canvas, evt) {
-		var rect = canvas.getBoundingClientRect();
-		return {
-			x: evt.clientX - rect.left,
-			y: evt.clientY - rect.top
-		};
-	}
-
-	function drawCanvas(canvas, mousePos, color, texture) {
-		var context = canvas.getContext('2d');
-
-		context.clearRect(0, 0, canvas.width, canvas.height);
-
-		context.fillStyle = color;
-		// use a rectangle because circles are antialiased
-		context.fillRect(mousePos.x - 12, mousePos.y - 12, 24, 24);
-
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-		gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas);
-		gl.bindTexture(gl.TEXTURE_2D, null);
-	}
-
-	function draw(canvas, evt) {
-		var mousePos = getMousePos(canvas, evt);
-
-		drawCanvas(painter, mousePos, color, rectTexture);
-		drawCanvas(mask, mousePos, 'rgba(255, 255, 255, 1.0)', maskTexture);
-
-		updateInput = true;
-	}
-
-	function handleStart(evt) {
-		evt.preventDefault();
-
-		down = true;
-
-		if (evt.shiftKey) { // wall
-			color = 'rgba(127, 127, 127, 0.5)';
-		} else if (evt.ctrlKey) { // empty
-			color = 'rgba(0, 0, 0, 0.5)';
-		} else if (evt.altKey) { // fire
-			color = 'rgba(255, 0, 0, 0.1)';
-		} else { // sand
-			color = 'rgba(255, 255, 255, 1.0)';
-		}
-
-		draw(canvas, evt);
-	}
-
-	function handleEnd(evt) {
-		evt.preventDefault();
-
-		down = false;
-	}
-
-	function handleMove(evt) {
-		evt.preventDefault();
-
-		if (down) {
-			draw(canvas, evt);
-		}
-	}
-
-	canvas.addEventListener('mousedown', handleStart, true);
-	canvas.addEventListener('mouseup', handleEnd, true);
-	canvas.addEventListener('mouseleave', handleEnd, true);
-	canvas.addEventListener('mousemove', handleMove, true);
-
-	canvas.addEventListener("touchstart", handleStart, true);
-	canvas.addEventListener("touchend", handleEnd, true);
-	canvas.addEventListener("touchcancel", handleEnd, true);
-	canvas.addEventListener("touchleave", handleEnd, true);
-	canvas.addEventListener("touchmove", handleMove, true);
 }
 
 //
@@ -162,7 +60,7 @@ function initUserInput() {
 // Initialize WebGL, returning the GL context or null if
 // WebGL isn't available or could not be initialized.
 //
-function initWebGL() {
+function initWebGL(canvas) {
 	gl = null;
 	
 	try {
@@ -226,56 +124,6 @@ function initBuffers() {
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
-function drawInput() {
-	/* add user input */
-
-	gl.useProgram(copyProgram);
-	
-	// Draw the rect by binding the array buffer to the rect's vertices
-	// array, setting attributes, and pushing it to GL.
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, rectVerticesBuffer);
-	gl.vertexAttribPointer(aCopyVertexPosition, 3, gl.FLOAT, false, 0, 0);
-
-	// Set the texture coordinates attribute for the vertices.
-	
-	gl.bindBuffer(gl.ARRAY_BUFFER, rectVerticesTextureCoordBuffer);
-	gl.vertexAttribPointer(aCopyTextureCoord, 2, gl.FLOAT, false, 0, 0);
-
-	// Specify the texture to map onto the face.
-	gl.uniform1i(uCopySampler, 0);
-
-	// draw mask
-	gl.blendFunc(gl.ZERO, gl.ONE_MINUS_SRC_ALPHA);
-
-	// draw user input mask
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, maskTexture);
-
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-	// preserve alpha
-	gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
-
-	// draw user input
-	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, rectTexture);
-
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-
-	// clear mask
-	gl.bindFramebuffer(gl.FRAMEBUFFER, maskFrameBuffer);
-
-	gl.clearColor(0.0, 0.0, 0.0, 0.0);  // Clear to transparent
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	// clear user input
-	gl.bindFramebuffer(gl.FRAMEBUFFER, rectFrameBuffer);
-
-	gl.clearColor(0.0, 0.0, 0.0, 0.0);  // Clear to transparent
-	gl.clear(gl.COLOR_BUFFER_BIT);
-}
-
 //
 // advance
 //
@@ -333,14 +181,6 @@ function advance() {
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
-function engine() {
-	advance();
-
-	if (updateInput) {
-		drawInput();
-	}
-}
-
 function drawScene() {
 	/* copy framebuffer to screen */
 
@@ -375,8 +215,6 @@ function drawScene() {
 
 	// restore color mask
 	gl.colorMask(true, true, true, true);
-
-	window.requestAnimationFrame(drawScene);
 }
 
 //
@@ -490,10 +328,33 @@ function getShader(gl, id) {
 	return shader;
 }
 
-function initTextures() {
-	var rectImage = new Image();
-	rectImage.onload = function() { handleTextureLoaded(rectImage); }
-	rectImage.src = './sand.png';
+function initTextures(canvas, config) {
+	var tempCanvas = document.createElement('canvas');
+
+	tempCanvas.width = canvas.width;
+	tempCanvas.height = canvas.height;
+
+	var context = tempCanvas.getContext('2d');
+
+	// keep buffers empty
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	
+	tex = initTextureWithFrameBuffer(canvas);
+	sandTexture0 = tex[0];
+	sandFrameBuffer0 = tex[1];
+	
+	tex = initTextureWithFrameBuffer(canvas);
+	sandTexture1 = tex[0];
+	sandFrameBuffer1 = tex[1];
+
+	// load config
+	var generator = new lutGenerator();
+	generator.parse(config);
+
+	cellsTexture = initTexture(generator.cellsData, false);
+	rulesTexture = initTexture(generator.rulesData, false);
+
+	gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
 function initTexture(image, flip) {
@@ -520,58 +381,4 @@ function initTextureWithFrameBuffer(image) {
 	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
 
 	return [texture, framebuffer];
-}
-
-function handleTextureLoaded(image) {
-	var canvas = document.createElement('canvas');
-
-	canvas.width = image.width;
-	canvas.height = image.height;
-
-	var context = canvas.getContext('2d');
-
-	// 'empty' has a density of 0.5
-	context.fillStyle = 'rgba(0, 0, 0, 0.5)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	context.drawImage(image, 0, 0);
-
-	var tex = initTextureWithFrameBuffer(canvas);
-	rectTexture = tex[0];
-	rectFrameBuffer = tex[1];
-
-	// keep buffers empty
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	
-	tex = initTextureWithFrameBuffer(canvas);
-	sandTexture0 = tex[0];
-	sandFrameBuffer0 = tex[1];
-	
-	tex = initTextureWithFrameBuffer(canvas);
-	sandTexture1 = tex[0];
-	sandFrameBuffer1 = tex[1];
-
-	// create mask, draw the entire image
-	context.fillStyle = 'rgba(255, 255, 255, 1.0)';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	tex = initTextureWithFrameBuffer(canvas);
-	maskTexture = tex[0];
-	maskFrameBuffer = tex[1];
-
-	// load config
-	var generator = new lutGenerator();
-	generator.parse(document.getElementById('config').textContent);
-
-	cellsTexture = initTexture(generator.cellsData, false);
-	rulesTexture = initTexture(generator.rulesData, false);
-
-	gl.bindTexture(gl.TEXTURE_2D, null);
-
-	// tell the engine to update the buffers with input
-	updateInput = true;
-
-	setInterval(engine, 8);
-
-	// Set up to draw the scene periodically.
-	window.requestAnimationFrame(drawScene);
 }
---
+++ js/user.js
@@ -0,0 +1,217 @@
+// textures
+var rectTexture, maskTexture;
+
+// frame buffers
+var rectFrameBuffer, maskFrameBuffer;
+
+// draw input hint
+var updateInput = false;
+
+function start() {
+	var canvas = document.getElementById('glcanvas');
+	var config = document.getElementById('config').textContent;
+
+	init(canvas, config);
+
+	if (gl) {
+		var rectImage = new Image();
+		rectImage.onload = function() { handleTextureLoaded(rectImage); }
+		rectImage.src = './sand.png';
+
+		initUserInput(canvas);
+		
+		window.requestAnimationFrame(drawScene);
+	}
+}
+
+function handleTextureLoaded(image) {
+	var canvas = document.createElement('canvas');
+
+	canvas.width = image.width;
+	canvas.height = image.height;
+
+	var context = canvas.getContext('2d');
+
+	// 'empty' has a density of 0.5
+	context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.drawImage(image, 0, 0);
+
+	var tex = initTextureWithFrameBuffer(canvas);
+	rectTexture = tex[0];
+	rectFrameBuffer = tex[1];
+
+	// create mask, draw the entire image
+	context.fillStyle = 'rgba(255, 255, 255, 1.0)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	tex = initTextureWithFrameBuffer(canvas);
+	maskTexture = tex[0];
+	maskFrameBuffer = tex[1];
+
+	gl.bindTexture(gl.TEXTURE_2D, null);
+
+	// tell the engine to update the buffers with input
+	updateInput = true;
+
+	setInterval(engine, 8);
+
+	// Set up to draw the scene periodically.
+	window.requestAnimationFrame(animate);	
+}
+
+function animate() {
+	drawScene();
+
+	window.requestAnimationFrame(animate);
+}
+
+function initUserInput(canvas) {
+	var down = false;
+	var color = 'rgba(255, 255, 255, 1.0)';
+
+	// create a new canvas
+	var painter = document.createElement('canvas');
+	var mask = document.createElement('canvas');
+
+	// set dimensions
+	mask.width = canvas.width;
+	mask.height = canvas.height;
+
+	painter.width = canvas.width;
+	painter.height = canvas.height;
+
+	function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+			x: evt.clientX - rect.left,
+			y: evt.clientY - rect.top
+		};
+	}
+
+	function drawCanvas(canvas, mousePos, color, texture) {
+		var context = canvas.getContext('2d');
+
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		context.fillStyle = color;
+		// use a rectangle because circles are antialiased
+		context.fillRect(mousePos.x - 12, mousePos.y - 12, 24, 24);
+
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas);
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	}
+
+	function draw(canvas, evt) {
+		var mousePos = getMousePos(canvas, evt);
+
+		drawCanvas(painter, mousePos, color, rectTexture);
+		drawCanvas(mask, mousePos, 'rgba(255, 255, 255, 1.0)', maskTexture);
+
+		updateInput = true;
+	}
+
+	function handleStart(evt) {
+		evt.preventDefault();
+
+		down = true;
+
+		if (evt.shiftKey) { // wall
+			color = 'rgba(127, 127, 127, 0.5)';
+		} else if (evt.ctrlKey) { // empty
+			color = 'rgba(0, 0, 0, 0.5)';
+		} else if (evt.altKey) { // fire
+			color = 'rgba(255, 0, 0, 0.1)';
+		} else { // sand
+			color = 'rgba(255, 255, 255, 1.0)';
+		}
+
+		draw(canvas, evt);
+	}
+
+	function handleEnd(evt) {
+		evt.preventDefault();
+
+		down = false;
+	}
+
+	function handleMove(evt) {
+		evt.preventDefault();
+
+		if (down) {
+			draw(canvas, evt);
+		}
+	}
+
+	canvas.addEventListener('mousedown', handleStart, true);
+	canvas.addEventListener('mouseup', handleEnd, true);
+	canvas.addEventListener('mouseleave', handleEnd, true);
+	canvas.addEventListener('mousemove', handleMove, true);
+
+	canvas.addEventListener("touchstart", handleStart, true);
+	canvas.addEventListener("touchend", handleEnd, true);
+	canvas.addEventListener("touchcancel", handleEnd, true);
+	canvas.addEventListener("touchleave", handleEnd, true);
+	canvas.addEventListener("touchmove", handleMove, true);
+}
+
+function drawInput() {
+	/* add user input */
+
+	gl.useProgram(copyProgram);
+	
+	// Draw the rect by binding the array buffer to the rect's vertices
+	// array, setting attributes, and pushing it to GL.
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, rectVerticesBuffer);
+	gl.vertexAttribPointer(aCopyVertexPosition, 3, gl.FLOAT, false, 0, 0);
+
+	// Set the texture coordinates attribute for the vertices.
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, rectVerticesTextureCoordBuffer);
+	gl.vertexAttribPointer(aCopyTextureCoord, 2, gl.FLOAT, false, 0, 0);
+
+	// Specify the texture to map onto the face.
+	gl.uniform1i(uCopySampler, 0);
+
+	// draw mask
+	gl.blendFunc(gl.ZERO, gl.ONE_MINUS_SRC_ALPHA);
+
+	// draw user input mask
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, maskTexture);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+	// preserve alpha
+	gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+
+	// draw user input
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, rectTexture);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+	// clear mask
+	gl.bindFramebuffer(gl.FRAMEBUFFER, maskFrameBuffer);
+
+	gl.clearColor(0.0, 0.0, 0.0, 0.0);  // Clear to transparent
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	// clear user input
+	gl.bindFramebuffer(gl.FRAMEBUFFER, rectFrameBuffer);
+
+	gl.clearColor(0.0, 0.0, 0.0, 0.0);  // Clear to transparent
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function engine() {
+	advance();
+
+	if (updateInput) {
+		drawInput();
+	}
+}
