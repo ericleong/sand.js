eb2df2ab77d8a222d0df97eca8951cd862a2695a
Eric Leong <eric@ericleong.me>
2014-12-26 21:32:35 -0500

    Add utility file.

---
+++ js/utils.js
@@ -0,0 +1,108 @@
+var SandUtils = {};
+
+SandUtils.createProgram = function(vertexShaderId, fragmentShaderId) {
+
+	var vertexShader = SandUtils.getShader(gl, vertexShaderId);
+	var fragmentShader = SandUtils.getShader(gl, fragmentShaderId);
+	
+	// Create the shader programs
+	
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	
+	// If creating the shader program failed, alert
+	
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		alert('Unable to initialize the shader program.');
+	}
+
+	return program;
+}
+
+//
+// getShader
+//
+// Loads a shader program by scouring the current document,
+// looking for a script with the specified ID.
+//
+SandUtils.getShader = function(gl, id) {
+	var shaderScript = document.getElementById(id);
+	
+	// Didn't find an element with the specified ID; abort.
+	
+	if (!shaderScript) {
+		return null;
+	}
+	
+	// Walk through the source element's children, building the
+	// shader source string.
+	
+	var theSource = '';
+	var currentChild = shaderScript.firstChild;
+	
+	while(currentChild) {
+		if (currentChild.nodeType == 3) {
+			theSource += currentChild.textContent;
+		}
+		
+		currentChild = currentChild.nextSibling;
+	}
+	
+	// Now figure out what type of shader script we have,
+	// based on its MIME type.
+	
+	var shader;
+	
+	if (shaderScript.type == 'x-shader/x-fragment') {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	} else if (shaderScript.type == 'x-shader/x-vertex') {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	} else {
+		return null;  // Unknown shader type
+	}
+	
+	// Send the source to the shader object
+	
+	gl.shaderSource(shader, theSource);
+	
+	// Compile the shader program
+	
+	gl.compileShader(shader);
+	
+	// See if it compiled successfully
+	
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+		return null;
+	}
+	
+	return shader;
+}
+
+SandUtils.initTexture = function(image, flip) {
+	flip = flip === undefined ? true : flip;
+
+	var texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flip);
+	gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+	return texture;
+}
+
+SandUtils.initTextureWithFrameBuffer = function(image) {
+	var texture = SandUtils.initTexture(image);
+
+	var framebuffer = gl.createFramebuffer();
+	gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+	gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+
+	return [texture, framebuffer];
+}
